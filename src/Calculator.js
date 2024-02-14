59c503b70a549069e8e797a5a50f4bed62958a5f
Nada Ahmed <nada.aly5613@gmail.com>
2024-02-14 21:50:01 +0200

    adding Inc and Dec buttons and enable sound effect

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -6,17 +6,29 @@ function Calculator({ workouts, allowSound }) {
   const [sets, setSets] = useState(3);
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
+  const [duration, setDuration] = useState(0);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+
+  const handleInc = ()=> {
+    setDuration(duration=> Math.floor(duration) + 1)
+  }
+  const handleDec = ()=> {
+    setDuration(duration=> duration > 1 ? Math.ceil(duration) - 1 : 0)
+  }
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
+ const playSound = useCallback( function () {
     if (!allowSound) return;
     const sound = new Audio(clickSound);
     sound.play();
-  };
+  }, [allowSound])
 
+  useEffect(()=>{
+    setDuration(duration=>(number * sets * speed) / 60 + (sets - 1) * durationBreak)
+    playSound()
+  },[number, sets,speed, durationBreak, playSound])
   return (
     <>
       <form>
@@ -66,13 +78,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={handleDec}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={handleInc}>+</button>
       </section>
     </>
   );
